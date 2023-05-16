c86b76744e7931ef563397369e91f828b0e208a8
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-05-16 11:07:32 +0400

    12. Typing useState - Type Inference

--- react-ts-practice-2/src/App.tsx
+++ react-ts-practice-2/src/App.tsx
@@ -1,66 +1,10 @@
 import "./App.css";
-import Button from "./components/props/Events/ClickEvent/Button";
-import Button2 from "./components/props/Events/ClickEvent/Button2";
-import Button3 from "./components/props/Events/ClickEvent/Button3";
-import Input from "./components/props/Events/OnChange/Input";
-import Greet from "./components/props/Greet";
-import Heading from "./components/props/Children/Heading";
-import Oscar from "./components/props//Children/Oscar";
-import Person from "./components/props/Person";
-import PersonalList from "./components/props/PersonalList";
-import Status from "./components/props/Status";
-import Container from "./components/props/Styles/Container";
+import LoggedIn from "./components/useState/LoggedIn";
 
 function App() {
-	const personFullName = {
-		firstName: "John",
-		lastName: "Doe",
-	};
-
-	const nameList = [
-		{
-			firstName: "Bruce",
-			lastName: "Wayne",
-		},
-		{
-			firstName: "Klark",
-			lastName: "Kent",
-		},
-		{
-			firstName: "Princess",
-			lastName: "Robota",
-		},
-	];
-
 	return (
 		<div className="App">
-			<Greet name="Typescript" dayCount={8} isLoggedIn={true} />
-			<Person fullname={personFullName} />
-			<PersonalList names={nameList} />
-			<Status status="loading" />
-			<Heading>Children props as String</Heading>
-			<Oscar>
-				<Heading> Oscar Goes to Leonardo Dicaprio!</Heading>
-			</Oscar>
-			<Greet name="HTML" isLoggedIn={true} />
-			<Button
-				handleClick={() => {
-					console.log("button Clicked");
-				}}
-			/>
-			<Button2
-				handleClick2={(event) => {
-					console.log("button2 Clicked", event);
-				}}
-			/>
-			<Button3
-				handleClick3={(event, id) => {
-					console.log("button3 Clicked", event, id);
-				}}
-			/>
-			<Input value="" handleChange={(event) => console.log(event)} />
-
-			<Container styles={{ border: "1px solid red", padding: "1rem" }} />
+			<LoggedIn />
 		</div>
 	);
 }
