207da9a869dae66106426f8c43246f7ab6ac5cc4
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-05-19 10:44:20 +0400

    17. Type useRef

--- react-ts-practice-2/src/App.tsx
+++ react-ts-practice-2/src/App.tsx
@@ -1,13 +1,9 @@
 import "./App.css";
-import ThemeContextProvider from "./components/context/ThemeContext";
-import { Box } from "./components/context/Box";
 
 function App() {
 	return (
 		<div className="App">
-			<ThemeContextProvider>
-				<Box />
-			</ThemeContextProvider>
+			<h1>useRef</h1>
 		</div>
 	);
 }
